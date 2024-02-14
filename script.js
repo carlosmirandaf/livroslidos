e5b6869dab4868e93250070b4375730cfdb0f4b8
carlosmirandaf <carlosantonio_miranda@hotmail.com>
2024-02-14 13:47:36 -0300

    adicionando textos a serem ocultados

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('link1').addEventListener('click', function() {
+    var antaresText = document.getElementById('antaresText');
+    antaresText.style.display = (antaresText.style.display === 'none' ? 'block' : 'none');
+});
+
+document.getElementById('link2').addEventListener('click', function() {
+    var ilusoesText = document.getElementById('ilusoesText');
+    ilusoesText.style.display = (ilusoesText.style.display === 'none' ? 'block' : 'none');
+});
+
+// Adicione mais listeners para cada link, se necessário
