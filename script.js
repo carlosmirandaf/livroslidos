3d7be15a4e31c0a80ff0f088a3a5623a2a441215
carlosmirandaf <carlosantonio_miranda@hotmail.com>
2024-03-09 17:28:58 -0300

    adicionando "os maias" como livro lido

--- script.js
+++ script.js
@@ -8,4 +8,9 @@ document.getElementById('link2').addEventListener('click', function() {
     ilusoesText.style.display = (ilusoesText.style.display === 'none' ? 'block' : 'none');
 });
 
+document.getElementById('link3').addEventListener('click', function() {
+    var MaiasText = document.getElementById('MaiasText');
+    MaiasText.style.display = (MaiasText.style.display === 'none' ? 'block' : 'none');
+});
+
 // Adicione mais listeners para cada link, se necessário
